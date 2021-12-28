f3bbc33980d682451e24e9e580a3b5685de1aae0
NardBruh <90529954+NardBruh@users.noreply.github.com>
2021-12-28 00:43:10 -0300

    Wow

--- mods/stages/studio-blocks.lua
+++ mods/stages/studio-blocks.lua
@@ -19,13 +19,27 @@ function onCreate()
 	setScrollFactor('MGBoppers', 1.0, 1.0);
 	scaleObject('MGBoppers', 1.3, 1.3);
 	
+	makeAnimatedLuaSprite('AaronDead', 'AaronDead', -175, 610);
+	addAnimationByPrefix('AaronDead','bop','AaronBop',24,false)
+	setScrollFactor('AaronDead', 1.0, 1.0);
+	scaleObject('AaronDead', 1.1, 1.1);
+
+	makeAnimatedLuaSprite('FGBoppers', 'FGBoppers', -500, 550);
+	addAnimationByPrefix('FGBoppers','bop','fganim',24,false)
+	setScrollFactor('FGBoppers', 1.0, 1.0);
+	scaleObject('FGBoppers', 1.3, 1.3);
+	
 	addLuaSprite('galaxiibgbackblocks', false);
 	addLuaSprite('BGBoppers', false);
 	addLuaSprite('galaxiibgfrontblocks', false);
 	addLuaSprite('MGBoppers', false);
+	addLuaSprite('AaronDead', true);
+	addLuaSprite('FGBoppers', true);
 end
 
 function onBeatHit()
 	objectPlayAnimation('BGBoppers','bop',false)
 	objectPlayAnimation('MGBoppers','bop',false)
+	objectPlayAnimation('FGBoppers','bop',false)
+	objectPlayAnimation('AaronDead','bop',false)
 end
---
+++ mods/stages/studio-post-gears.lua
@@ -0,0 +1,23 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('galaxiibgbackblocks', 'galaxiibgbackblocks', -600, -550);
+	setScrollFactor('galaxiibgbackblocks', 1.0, 1.0);
+	scaleObject('galaxiibgbackblocks', 1.1, 1.1);
+	
+	makeLuaSprite('galaxiibgfrontblocks', 'galaxiibgfrontblocks', -600, -550);
+	setScrollFactor('galaxiibgfrontblocks', 1.0, 1.0);
+	scaleObject('galaxiibgfrontblocks', 1.1, 1.1);
+
+	makeAnimatedLuaSprite('AaronDead', 'AaronDead', -175, 610);
+	addAnimationByPrefix('AaronDead','bop','AaronBop',24,false)
+	setScrollFactor('AaronDead', 1.0, 1.0);
+	scaleObject('AaronDead', 1.1, 1.1);
+		
+	addLuaSprite('galaxiibgbackblocks', false);
+	addLuaSprite('galaxiibgfrontblocks', false);
+	addLuaSprite('AaronDead', true);
+end
+	
+function onBeatHit()
+	objectPlayAnimation('AaronDead','bop',false)
+end
