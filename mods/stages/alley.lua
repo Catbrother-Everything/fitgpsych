8820af1f2144153fda8680ee10c91fe295a6c43f
NardBruh <90529954+NardBruh@users.noreply.github.com>
2021-12-29 19:50:32 -0300

    Alley

---
+++ mods/stages/alley.lua
@@ -0,0 +1,62 @@
+local birdX = math.random(600, 950);
+local birdY = math.random(-100, -250);
+
+function onCreate()
+	-- background shit
+	makeLuaSprite('Sky', 'Sky', -600, -550);
+	setScrollFactor('Sky', 1.0, 1.0);
+	scaleObject('Sky', 1.0, 1.0);
+
+	makeAnimatedLuaSprite('clouds', 'clouds', -2000, -550);
+	setScrollFactor('clouds', 1.0, 1.0);
+	addAnimationByPrefix('clouds','bop','CloudsIdle',24,false)
+	scaleObject('clouds', 1.1, 1.1);
+
+	makeAnimatedLuaSprite('Birds', 'Birds', birdX, birdY);
+	setScrollFactor('Birds', 0.02, 0.02);
+	addAnimationByPrefix('Birds','bop','birdie',24,true)
+	scaleObject('Birds', 0.4, 0.4);
+
+	makeLuaSprite('BGBuildings', 'BGBuildings', -220, -280);
+	setScrollFactor('BGBuildings', 1.0, 1.0);
+	scaleObject('BGBuildings', 0.7, 0.7);
+
+	makeLuaSprite('Road', 'Road', -600, -430);
+	setScrollFactor('Road', 1.0, 1.0);
+	scaleObject('Road', 0.9, 0.9);
+
+	makeLuaSprite('Fence', 'Fence', 10, -530);
+	setScrollFactor('Fence', 1.0, 1.0);
+	scaleObject('Fence', 1.1, 1.1);
+
+	makeLuaSprite('Walls', 'Walls', -725, -790);
+	setScrollFactor('Walls', 1.0, 1.0);
+	scaleObject('Walls', 1.08, 1.08);
+
+	makeLuaSprite('Ground', 'Ground', -550, -520);
+	setScrollFactor('Ground', 1.0, 1.0);
+	scaleObject('Ground', 1.06, 1.06);
+
+	makeLuaSprite('Dumpsters', 'Dumpsters', -350, -475);
+	setScrollFactor('Dumpsters', 1.0, 1.0);
+	scaleObject('Dumpsters', 1.0, 1.0);
+	
+	makeLuaSprite('Trash', 'Trash', -300, -440);
+	setScrollFactor('Trash', 1.0, 1.0);
+	scaleObject('Trash', 0.96, 0.96);
+		
+	addLuaSprite('Sky', false);
+	addLuaSprite('clouds', false);
+	addLuaSprite('Birds', false);
+	addLuaSprite('BGBuildings', false);
+	addLuaSprite('Road', false);
+	addLuaSprite('Fence', false);
+	addLuaSprite('Walls', false);
+	addLuaSprite('Ground', false);
+	addLuaSprite('Dumpsters', false);
+	addLuaSprite('Trash', false);
+end
+
+function onBeatHit()
+	objectPlayAnimation('clouds','bop',false)
+end
