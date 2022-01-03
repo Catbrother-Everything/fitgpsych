f5874a13e0c2d06e5278c95d75ec841fcd8774e1
NardBruh <90529954+NardBruh@users.noreply.github.com>
2022-01-03 17:24:02 -0300

    Uhhhhhh

--- mods/custom_notetypes/Static Note.lua
+++ mods/custom_notetypes/Static Note.lua
@@ -1,3 +1,5 @@
+local hitten = 0
+
 function onCreate()
 	--Iterate over all notes
 	for i = 0, getProperty('unspawnNotes.length')-1 do
@@ -13,10 +15,106 @@ function onCreate()
 			end
 		end
 	end
+
+	makeAnimatedLuaSprite('virgin', 'virginity_assets', -113, -70);
+	setScrollFactor('virgin', 0.0, 0.0);
+	addAnimationByPrefix('virgin','bop','fnafstatic',24,true)
+	scaleObject('virgin', 1.2, 1.2);
+
+	makeAnimatedLuaSprite('virgin2', 'virginity_assets', -113, -70);
+	setScrollFactor('virgin2', 0.0, 0.0);
+	addAnimationByPrefix('virgin2','bop','fnafstatic',24,true)
+	scaleObject('virgin2', 1.2, 1.2);
+
+	makeAnimatedLuaSprite('virgin3', 'virginity_assets', -113, -70);
+	setScrollFactor('virgin3', 0.0, 0.0);
+	addAnimationByPrefix('virgin3','bop','fnafstatic',24,true)
+	scaleObject('virgin3', 1.2, 1.2);
+
+	makeAnimatedLuaSprite('virgin4', 'virginity_assets', -113, -70);
+	setScrollFactor('virgin4', 0.0, 0.0);
+	addAnimationByPrefix('virgin4','bop','fnafstatic',24,true)
+	scaleObject('virgin4', 1.2, 1.2);
+
+	makeAnimatedLuaSprite('virgin5', 'virginity_assets', -113, -70);
+	setScrollFactor('virgin5', 0.0, 0.0);
+	addAnimationByPrefix('virgin5','bop','fnafstatic',24,true)
+	scaleObject('virgin5', 1.2, 1.2);
+
+	makeAnimatedLuaSprite('virgin6', 'virginity_assets', -113, -70);
+	setScrollFactor('virgin6', 0.0, 0.0);
+	addAnimationByPrefix('virgin6','bop','fnafstatic',24,true)
+	scaleObject('virgin6', 1.2, 1.2);
+
+	makeAnimatedLuaSprite('virgin7', 'virginity_assets', -113, -70);
+	setScrollFactor('virgin7', 0.0, 0.0);
+	addAnimationByPrefix('virgin7','bop','fnafstatic',24,true)
+	scaleObject('virgin7', 1.2, 1.2);
+
+	makeAnimatedLuaSprite('virgin8', 'virginity_assets', -113, -70);
+	setScrollFactor('virgin8', 0.0, 0.0);
+	addAnimationByPrefix('virgin8','bop','fnafstatic',24,true)
+	scaleObject('virgin8', 1.2, 1.2);
+
+	makeAnimatedLuaSprite('virgin9', 'virginity_assets', -113, -70);
+	setScrollFactor('virgin9', 0.0, 0.0);
+	addAnimationByPrefix('virgin9','bop','fnafstatic',24,true)
+	scaleObject('virgin9', 1.2, 1.2);
+
+	makeAnimatedLuaSprite('virgin10', 'virginity_assets', -113, -70);
+	setScrollFactor('virgin10', 0.0, 0.0);
+	addAnimationByPrefix('virgin10','bop','fnafstatic',24,true)
+	scaleObject('virgin10', 1.2, 1.2);
 end
 
--- Function called when you hit a note (after note hit calculations)
--- id: The note member id, you can get whatever variable you want from this note, example: "getPropertyFromGroup('notes', id, 'strumTime')"
--- noteData: 0 = Left, 1 = Down, 2 = Up, 3 = Right
--- noteType: The note type string/tag
--- isSustainNote: If it's a hold note, can be either true or false
+function noteMiss(id, noteData, noteType, isSustainNote)
+	if noteType == 'Static Note' then
+		hitten = hitten + 1;
+	end
+end
+
+function onUpdate(elapsed)
+	if hitten == 1 then
+		addLuaSprite('virgin', true);
+	end
+
+	if hitten == 2 then
+		addLuaSprite('virgin2', true);
+	end
+
+	if hitten == 3 then
+		addLuaSprite('virgin3', true);
+	end
+
+	if hitten == 4 then
+		addLuaSprite('virgin4', true);
+	end
+
+	if hitten == 5 then
+		addLuaSprite('virgin5', true);
+	end
+
+	if hitten == 6 then
+		addLuaSprite('virgin6', true);
+	end
+
+	if hitten == 7 then
+		addLuaSprite('virgin7', true);
+	end
+
+	if hitten == 8 then
+		addLuaSprite('virgin8', true);
+	end
+
+	if hitten == 9 then
+		addLuaSprite('virgin9', true);
+	end
+
+	if hitten == 10 then
+		addLuaSprite('virgin10', true);
+	end
+
+	if hitten >= 11 then
+		setProperty('health', 0);
+	end
+end
--- mods/stages/hacz.lua
+++ mods/stages/hacz.lua
@@ -2,10 +2,15 @@ function onCreate()
 	-- background shit
 	makeAnimatedLuaSprite('static', 'static', -200, -350);
 	setScrollFactor('static', 1.0, 1.0);
-	addAnimationByPrefix('static','bop','static',24,loop)
+	addAnimationByPrefix('static','bop','static',24,true)
 	scaleObject('static', 1.9, 1.9);
 
+	makeLuaSprite('Vignette', 'Vignette', -34, -65);
+	setScrollFactor('Vignette', 0.0, 0.0);
+	scaleObject('Vignette', 1.41, 1.5);
+
 	addLuaSprite('static', false);
+	addLuaSprite('Vignette', true);
 end
 
 function onBeatHit()
