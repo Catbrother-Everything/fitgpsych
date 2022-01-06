4d1c408625daac4d710bc0b94e1ede487654f5d8
NardBruh <90529954+NardBruh@users.noreply.github.com>
2022-01-06 03:14:59 -0300

    Boss Fight

--- mods/data/boss-fight/script.lua
+++ mods/data/boss-fight/script.lua
@@ -7,8 +7,17 @@ function onCreate()
 end
 
 function onUpdate(elapsed)
+	if curStep == 368 then
+		doTweenAlpha('1','camHUD',0,0.5,'linear')
+	end
+
+	if curStep == 384 then
+		doTweenZoom('2','camGame',1.3,1.7,'linear')
+	end
+
 	if curStep == 400 then
-		setProperty('gf.x',350)
-		setProperty('gf.y',80)
+		setProperty('gf.x',350);
+		setProperty('gf.y',80);
+		doTweenAlpha('1','camHUD',1,0.5,'linear')
 	end
 end
