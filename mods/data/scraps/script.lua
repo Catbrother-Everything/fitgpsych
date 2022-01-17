3ddc5cbd156a1ca3757d3ac173a04f117df75c56
NardBruh <90529954+NardBruh@users.noreply.github.com>
2022-01-16 22:04:15 -0300

    Damn Scraps

--- mods/data/scraps/script.lua
+++ mods/data/scraps/script.lua
@@ -20,3 +20,31 @@ function goodNoteHit(d, noteData, noteType, isSustainNote)
 		objectPlayAnimation('Fuck','gun',true)
 	end
 end
+
+function opponentNoteHit()
+	setProperty('health',getProperty('health') - 0.01)
+end
+
+function onUpdate(elapsed)
+	if curStep == 255 or curStep == 320 or curStep == 543 or curStep == 1055 or curStep == 1144 then
+		setProperty('songSpeed',getProperty('songSpeed') + 0.1)
+	end
+
+	if curStep == 287 or curStep == 351 then
+		setProperty('songSpeed',getProperty('songSpeed') - 0.1)
+	end
+
+	if curStep == 447 then
+		setProperty('songSpeed',getProperty('songSpeed') + 0.2)
+	end
+
+	if curStep == 788 then
+		setProperty('songSpeed',getProperty('songSpeed') - 0.6)
+	end
+
+	if curStep == 927 then
+		setProperty('songSpeed',getProperty('songSpeed') + 0.3)
+	end
+
+
+end
