a0c286263b0d67fbfcc5d0b58d78d0e28271b6ac
NardBruh <90529954+NardBruh@users.noreply.github.com>
2022-01-22 15:56:54 -0300

    The modchart

--- mods/data/blocks/modchart.lua
+++ mods/data/blocks/modchart.lua
@@ -1,58 +1,36 @@
-function setDefault(id)
-	_G['defaultStrum'..id..'X'] = getActorX(id)
-end
+local confirmRise = false
+local junk = math.random(1,2)
+local lmao1 = math.random(20,40,60,80,100)
+local lmao2 = math.random(20,40,60,80,100)
+local lmao3 = math.random(20,40,60,80,100)
+local lmao4 = math.random(20,40,60,80,100)
+local lmao5 = math.random(20,40,60,80,100)
+local lmao6 = math.random(20,40,60,80,100)
+local lmao7 = math.random(20,40,60,80,100)
+local lmao8 = math.random(20,40,60,80,100)
 
-function start (song)
+function onUpdate(elapsed)
+	if confirmRise == false and curStep >= 480 and curStep < 1014 then
+		noteTweenY('4',4,0,0.033,'circInOut')
+		noteTweenY('5',5,0,0.033,'circInOut')
+		noteTweenY('6',6,0,0.033,'circInOut')
+		noteTweenY('7',7,0,0.033,'circInOut')
+		noteTweenY('0',0,0,0.033,'circInOut')
+		noteTweenY('1',1,0,0.033,'circInOut')
+		noteTweenY('2',2,0,0.033,'circInOut')
+		noteTweenY('3',3,0,0.033,'circInOut')
+		confirmRise = true
+	end
 
-end
-
-function update (elapsed)
-local currentBeat = (songPos / 1000)*(bpm/60)	
-	if bounce then 
-		for i=0,7 do
-			setActorY(_G['defaultStrum'..i..'Y'] + 12 * math.sin((currentBeat*2 * math.pi)), i)
-		end
-	end
-	if morebounce then 
-		for i=0,7 do
-			setActorY(_G['defaultStrum'..i..'Y'] + 16 * math.sin((currentBeat*2 * math.pi)), i)
-		end
-	end
-	if lowbounce then 
-		for i=0,7 do
-			setActorY(_G['defaultStrum'..i..'Y'] + 4 * math.sin((currentBeat*2 * math.pi)), i)
-		end
-	end
-	if cambounce then
-	camHudAngle = camHudAngle - 0.08 * math.cos((currentBeat*0.5 * math.pi))
-	end
-	if slowcambounce then
-	camHudAngle = camHudAngle - 0.16 * math.cos((currentBeat*0.5 * math.pi))
-	end
-	if default then
-		for i=0,7 do
-			setActorX(_G['defaultStrum'..i..'X'],i)
-			setActorY(_G['defaultStrum'..i..'Y'],i)
-        end
-	end
-end
-
-function stepHit (step)
-	if step == 480 then
-		morebounce = true
-	end
-	if step == 608 then
-		bounce = true
-                morebounce = false
-	end
-	if difficulty == 2 and step == 736 then
-            morebounce = true
-            cambounce = true
-            bounce = false
-	end
-	if step == 1049 then
-            default = true
-            morebounce = false
-            cambounce = false
+	if confirmRise == true and curStep >= 480 and curStep < 1014 then
+		noteTweenY('12',4,lmao1,0.033,'circInOut')
+		noteTweenY('13',5,lmao2,0.033,'circInOut')
+		noteTweenY('14',6,lmao3,0.033,'circInOut')
+		noteTweenY('15',7,lmao4,0.033,'circInOut')
+		noteTweenY('8',0,lmao5,0.033,'circInOut')
+		noteTweenY('9',1,lmao6,0.033,'circInOut')
+		noteTweenY('10',2,lmao7,0.033,'circInOut')
+		noteTweenY('11',3,lmao8,0.033,'circInOut')
+		confirmRise = false
 	end
 end
