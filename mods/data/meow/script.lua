7dc93ee8ec74828f3bbfb3dfb96a5b85d46b8d75
NardBruh <90529954+NardBruh@users.noreply.github.com>
2022-02-02 20:58:04 -0300

    Take this

--- mods/data/meow/script.lua
+++ mods/data/meow/script.lua
@@ -1,16 +1,22 @@
 function onCreate()
-    if curStep == 0 then
-        makeLuaSprite('black', 'black', -250, -200);
-	    setScrollFactor('black', 0.0, 0.0);
-	    scaleObject('black', 3.0, 3.0);
-    end
+    makeLuaSprite('black', 'black', -250, -200);
+    setScrollFactor('black', 0.0, 0.0);
+    scaleObject('black', 3.0, 3.0);
+    addCharacterToList('mouse', 'dad')
 end
 
 function onUpdate(elapsed)
     if curStep == 336 then
         addLuaSprite('black',true)
+        doTweenAlpha('iconoftheday','iconP2',0,1,'linear')
     end
     if curStep == 416 then
         removeLuaSprite('black', true);
+        triggerEvent('Change Character', 1, 'mouse');
+        doTweenAlpha('iconoftheday','iconP2',1,1,'linear')
+    end
+
+    if curStep >= 338 and curStep <= 414 and getProperty('health') > 1 then
+        setProperty('health',getProperty('health') - 0.01)
     end
 end
