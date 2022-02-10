1eb0f89a5be9f759025fd27720d30906cfaf0784
Lil-Parrot <chaznmouse@yahoo.com>
2022-02-10 14:52:51 -0600

    Self Doubt is finished (doubt bf my beloved)

---
+++ mods/stages/meow87.lua
@@ -0,0 +1,8 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('galaxiibgbackblocks', 'MeowBG2', -600, -550);
+	setScrollFactor('galaxiibgbackblocks', 1.0, 1.0);
+	scaleObject('galaxiibgbackblocks', 1.1, 1.1);
+
+	addLuaSprite('galaxiibgbackblocks', false);
+end
