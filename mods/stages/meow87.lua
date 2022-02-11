eaa4f91237cd39d6ed5fcfdabe4788a664396037
NardBruh <90529954+NardBruh@users.noreply.github.com>
2022-02-10 22:06:33 -0300

    Damn

--- mods/stages/meow87.lua
+++ mods/stages/meow87.lua
@@ -1,8 +1,8 @@
 function onCreate()
 	-- background shit
-	makeLuaSprite('galaxiibgbackblocks', 'MeowBG2', -600, -550);
+	makeLuaSprite('galaxiibgbackblocks', 'MeowBG2', -450, -400);
 	setScrollFactor('galaxiibgbackblocks', 1.0, 1.0);
-	scaleObject('galaxiibgbackblocks', 1.1, 1.1);
+	scaleObject('galaxiibgbackblocks', 1.0, 1.0);
 
 	addLuaSprite('galaxiibgbackblocks', false);
 end
