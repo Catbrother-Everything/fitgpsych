0a5ba3587ecbd0047aece874ed6801d1924ac778
NardBruh <90529954+NardBruh@users.noreply.github.com>
2022-02-14 23:32:34 -0300

    Freggy

--- mods/data/hacks/script.lua
+++ mods/data/hacks/script.lua
@@ -1,4 +1,11 @@
-local whichText = 7
+local junk = math.random(0,1)
+local onScreen = false
+local waitingTime = 0
+local posX = 0
+local posY = 0
+local whichText = 13
+local staticHit = 0
+local fuck = false
 
 function onCreate()
 	if isStoryMode then
@@ -16,43 +23,55 @@ function onUpdate()
 	if curStep == 1 then
 		doTweenAlpha('1','cutscene',0,5,'circInOut')
 	end
-	setTextFont('HELP', 'HelpMe.tff')
-
 	if curStep == 33 then
 		removeLuaObject('cutscene',true)
 	end
-	if whichText == 0 then
-		setTextString('HELP', 'THE VOICES CLIP')
-	end
-	if whichText == 1 then
-		setTextString('HELP', 'ITS SO COLD')
-	end
-	if whichText == 2 then
-		setTextString('HELP', 'HES NEAR')
-	end
-	if whichText == 3 then
-		setTextString('HELP', 'GET OUT')
-	end
-	if whichText == 4 then
-		setTextString('HELP', 'MOVE')
-	end
-	if whichText == 5 then
-		setTextString('HELP', 'OUT OF THE WAY')
-	end
-	if whichText == 6 then
-		setTextString('HELP', 'https://cdn.discordapp.com/attachments/864663683686727683/942910573245526046/FLiiDtNXsAEPg6i.png')
-	end
 end
 
-function onBeatHit()
-	if curBeat == 72 then
-		makeLuaText('HELP', 'I CANT LOSE', 350, 0, 450)
-		setTextFont('HELP', 'HelpMe.tff')
-		setTextAlignment('HELP', 'center')
-		setTextSize('HELP', 100)
-		addLuaText('HELP')
-	end
-	if curBeat > 72 then
-		whichText = math.random(0, 6)
+function noteMiss(id, noteData, noteType, isSustainNote)
+	if noteType == 'Static Note' then
+		whichText = math.random(0,11)
+		posX = math.random(0,860)
+		posY = math.random(0,650)
+		if whichText == 0 then
+			makeLuaSprite('text', 'halloween/cold', posX, posY);
+		end
+		if whichText == 1 then
+			makeLuaSprite('text', 'halloween/get_it', posX, posY);
+		end
+		if whichText == 2 then
+			makeLuaSprite('text', 'halloween/https', posX, posY);
+		end
+		if whichText == 3 then
+			makeLuaSprite('text', 'halloween/lose', posX, posY);
+		end
+		if whichText == 4 then
+			makeLuaSprite('text', 'halloween/move', posX, posY);
+		end
+		if whichText == 5 then
+			makeLuaSprite('text', 'halloween/near', posX, posY);
+		end
+		if whichText == 6 then
+			makeLuaSprite('text', 'halloween/out', posX, posY);
+		end
+		if whichText == 7 then
+			makeLuaSprite('text', 'halloween/pal', posX, posY);
+		end
+		if whichText == 8 then
+			makeLuaSprite('text', 'halloween/punishment', posX, posY);
+		end
+		if whichText == 9 then
+			makeLuaSprite('text', 'halloween/tiring', posX, posY);
+		end
+		if whichText == 10 then
+			makeLuaSprite('text', 'halloween/voices', posX, posY);
+		end
+		if whichText == 11 then
+			makeLuaSprite('text', 'halloween/way', posX, posY);
+		end
+		setScrollFactor('text', 0.0, 0.0);
+		scaleObject('text', 1.0, 1.0);
+		addLuaSprite('text',true)
+		setObjectCamera('text','camOther')
 	end
 end
