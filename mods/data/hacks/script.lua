bfbc3eb4b8d23fa15c70807890299eaa52dbd57b
NardBruh <90529954+NardBruh@users.noreply.github.com>
2022-03-17 19:51:52 -0300

    Incredible

--- mods/data/hacks/script.lua
+++ mods/data/hacks/script.lua
@@ -8,15 +8,13 @@ local staticHit = 0
 local fuck = false
 
 function onCreate()
-	if isStoryMode then
-		makeAnimatedLuaSprite('cutscene', 'virginity_assets', -113, -70);
-		setScrollFactor('cutscene', 0.0, 0.0);
-		addAnimationByPrefix('cutscene','bop','fnafstatic',24,true)
-		scaleObject('cutscene', 1.2, 1.2);
+	makeAnimatedLuaSprite('cutscene', 'virginity_assets', -113, -70);
+	setScrollFactor('cutscene', 0.0, 0.0);
+	addAnimationByPrefix('cutscene','bop','fnafstatic',24,true)
+	scaleObject('cutscene', 1.2, 1.2);
 
-		addLuaSprite('cutscene',true)
-		setObjectCamera('cutscene','camOther')
-	end
+	addLuaSprite('cutscene',true)
+	setObjectCamera('cutscene','camOther')
 end
 
 function onUpdate()
