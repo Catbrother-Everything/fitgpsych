f26b9b6b66d87d62c845ce7bc75ea712e62491ad
Lil-Parrot <chaznmouse@yahoo.com>
2022-04-15 21:41:39 -0500

    aas

--- mods/data/blocks/modchart.lua
+++ mods/data/blocks/modchart.lua
@@ -22,67 +22,6 @@ function onCreate()
 	end
 end
 
-function onUpdate(elapsed)
-	if curStep == 0 and wasDownScroll and not songEnded then
-		noteTweenY('54',2,20,0.7,'circInOut')
-	end
-
-	if curStep == 32 and wasDownScroll and not songEnded then
-		noteTweenY('55',0,20,0.7,'circInOut')
-	end
-
-	if curStep == 64 and wasDownScroll and not songEnded then
-		noteTweenY('56',3,20,0.7,'circInOut')
-	end
-
-	if curStep == 96 and wasDownScroll and not songEnded then
-		noteTweenY('57',1,20,0.7,'circInOut')
-	end
-
-	if curStep == 161 and wasDownScroll and not songEnded then
-		noteTweenY('57',4,20,0.7,'circInOut')
-		noteTweenY('58',5,20,0.7,'circInOut')
-		noteTweenY('59',6,20,0.7,'circInOut')
-		noteTweenY('60',7,20,0.7,'circInOut')
-		doTweenY('health', 'camOther', 20, 0.7, 'circInOut')
-	end
-
-	if curStep == 40 and not songEnded then
-		lmao1 = math.random(20,100)
-		lmao2 = math.random(20,100)
-		lmao3 = math.random(20,100)
-		lmao4 = math.random(20,100)
-		lmao5 = math.random(20,100)
-		lmao6 = math.random(20,100)
-		lmao7 = math.random(20,100)
-		lmao8 = math.random(20,100)
-	end
-
-	if confirmRise == false and curStep >= 480 and curStep < 1014 and not songEnded then
-		noteTweenY('4',4,0,0.033,'circInOut')
-		noteTweenY('5',5,0,0.033,'circInOut')
-		noteTweenY('6',6,0,0.033,'circInOut')
-		noteTweenY('7',7,0,0.033,'circInOut')
-		noteTweenY('0',0,0,0.033,'circInOut')
-		noteTweenY('1',1,0,0.033,'circInOut')
-		noteTweenY('2',2,0,0.033,'circInOut')
-		noteTweenY('3',3,0,0.033,'circInOut')
-		confirmRise = true
-	end
-
-	if confirmRise == true and curStep >= 480 and curStep < 1014 and not songEnded then
-		noteTweenY('12',4,lmao1,0.033,'circInOut')
-		noteTweenY('13',5,lmao2,0.033,'circInOut')
-		noteTweenY('14',6,lmao3,0.033,'circInOut')
-		noteTweenY('15',7,lmao4,0.033,'circInOut')
-		noteTweenY('8',0,lmao5,0.033,'circInOut')
-		noteTweenY('9',1,lmao6,0.033,'circInOut')
-		noteTweenY('10',2,lmao7,0.033,'circInOut')
-		noteTweenY('11',3,lmao8,0.033,'circInOut')
-		confirmRise = false
-	end
-end
-
 function onEndSong()
 	songEnded = true
 end
