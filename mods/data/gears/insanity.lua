cb18fc43cf5890e080ccc19365cd1986bd77191f
NardBruh <90529954+NardBruh@users.noreply.github.com>
2022-04-25 13:40:48 -0300

    Time to

--- mods/data/gears/insanity.lua
+++ mods/data/gears/insanity.lua
@@ -1,20 +1,22 @@
 function onCreate()
-    makeAnimatedLuaSprite('insanitygearedge', 'insanity/gears', -100, 0)
-    addAnimationByPrefix('insanitygearedge', 'corner gears', 'corner gearsB', 24, true)
-    objectPlayAnimation('insanitygearedge','corner gears', true)
-    scaleObject('insanitygearedge', 1.5, 1.5)
-    addLuaSprite('insanitygearedge', true)
-    setScrollFactor('insanitygearedge', 0.9, 0.9);
-    setObjectCamera('insanitygearedge', 'hud')
+    if week == 'zInsanity' then
+        makeAnimatedLuaSprite('insanitygearedge', 'insanity/gears', -100, 0)
+        addAnimationByPrefix('insanitygearedge', 'corner gears', 'corner gearsB', 24, true)
+        objectPlayAnimation('insanitygearedge','corner gears', true)
+        scaleObject('insanitygearedge', 1.5, 1.5)
+        addLuaSprite('insanitygearedge', true)
+        setScrollFactor('insanitygearedge', 0.9, 0.9);
+        setObjectCamera('insanitygearedge', 'hud')
 
-    makeAnimatedLuaSprite('insanitygearsweep', 'insanity/gearsweep', 9000, 100)
-    addAnimationByPrefix('insanitygearsweep', 'gearssweep', 'gearssweep', 24, true)
-    objectPlayAnimation('insanitygearsweep','gearssweep',true)
-    scaleObject('insanitygearsweep', 1.5, 1.5)
-    addLuaSprite('insanitygearsweep', true)
-    doTweenX('lamo', 'insanitygearsweep', 5000, 0.01, linear)
-    setObjectCamera('insanitygearsweep', 'hud')
-    runTimer('gearsweep', 3, 1)
+        makeAnimatedLuaSprite('insanitygearsweep', 'insanity/gearsweep', 9000, 100)
+        addAnimationByPrefix('insanitygearsweep', 'gearssweep', 'gearssweep', 24, true)
+        objectPlayAnimation('insanitygearsweep','gearssweep',true)
+        scaleObject('insanitygearsweep', 1.5, 1.5)
+        addLuaSprite('insanitygearsweep', true)
+        doTweenX('lamo', 'insanitygearsweep', 5000, 0.01, linear)
+        setObjectCamera('insanitygearsweep', 'hud')
+        runTimer('gearsweep', 3, 1)
+    end
 end
 
 function onSongStart()
