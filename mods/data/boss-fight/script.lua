88d9170cef53d9f419ad5abef86d3a71712b5d42
NardBruh <90529954+NardBruh@users.noreply.github.com>
2022-06-02 23:49:37 -0300

    fREGGY

--- mods/data/boss-fight/script.lua
+++ mods/data/boss-fight/script.lua
@@ -1,6 +1,7 @@
 function onCreate()
 	setProperty('dad.x',80)
 	setProperty('dad.y',180)
+	addCharacterToList('gf-and-her-gfs-awake', 'gf')
 end
 
 function onUpdate(elapsed)
@@ -12,7 +13,12 @@ function onUpdate(elapsed)
 		doTweenZoom('2','camGame',1.3,1.7,'linear')
 	end
 
+	if curStep == 396 then
+		characterPlayAnim('gf', 'scared', false)
+	end
+
 	if curStep == 400 then
 		doTweenAlpha('1','camHUD',1,0.5,'linear')
+		triggerEvent('Change Character', 'gf', 'gf-and-her-gfs-awake');
 	end
 end
