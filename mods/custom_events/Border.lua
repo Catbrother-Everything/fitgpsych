45ef3aa72bdd64ccf3b07d58192acaa54ee84396
Lil-Parrot <chaznmouse@yahoo.com>
2022-07-22 14:58:04 -0500

    fixed nards mistakes + hacks updates + gears update

--- mods/custom_events/Border.lua
+++ mods/custom_events/Border.lua
@@ -1,15 +1,24 @@
 -- Event notes hooks
---function onEvent(name, value1, value2)
-	--if name == 'Border' then
-		--duration = tonumber(value1);
-		--if duration < 0 then
-			--duration = 0;
-		--end
+function onEvent(name, value1, value2)
+	if name == 'Border' then
+		duration = tonumber(value1);
+		if duration < 0 then
+			duration = 0;
+		end
+		makeLuaSprite('blackBorderBottom', 'black border', 0, 0);
+		scaleObject('blackBorderBottom', 5, 0.9);
+		setObjectCamera('blackBorderBottom', 'other')
+		addLuaSprite('blackBorderBottom')
+		--lol
+		makeLuaSprite('blackBorderTop', 'black border', 0, 550);
+		scaleObject('blackBorderTop', 5, 0.9);
+		setObjectCamera('blackBorderTop', 'other')
+		addLuaSprite('blackBorderTop')
+		doTweenAlpha('bottomborderinvis', 'blackBorderTop', 0, 0.1,'linear')
+		doTweenAlpha('topborderinvis', 'blackBorderBottom', 0, 0.1,'linear')
 
-		--targetAlpha = tonumber(value2);
-		--doTweenAlpha('blackbarfadein', 'black border bottom', targetAlpha, duration, 'linear');
-		--doTweenY('gooMove','black border bottom',-50,18,'linear')
-		----removeLuaSprite('black border bottom')
-		--debugPrint('Event triggered: ', name, duration, targetAlpha);
-	--end
---end
+		targetAlpha = tonumber(value2);
+        doTweenAlpha('bottombordervisible', 'blackBorderTop', 1, 0.2,'linear')
+        doTweenAlpha('topbordervisible', 'blackBorderBottom', 1, 0.2,'linear')
+	end
+end
--- mods/data/hacks/script.lua
+++ mods/data/hacks/script.lua
@@ -28,6 +28,102 @@ function onUpdate()
 	end
 end
 
+function onBeatHit()
+	if curBeat > 72 then
+		whichText = math.random(0,11)
+		posX = math.random(0,860)
+		posY = math.random(0,650)
+		if whichText == 0 then
+			makeLuaSprite('text', 'halloween/cold', posX, posY);
+		end
+		if whichText == 1 then
+			makeLuaSprite('text', 'halloween/get_it', posX, posY);
+		end
+		if whichText == 2 then
+			makeLuaSprite('text', 'halloween/https', posX, posY);
+		end
+		if whichText == 3 then
+			makeLuaSprite('text', 'halloween/lose', posX, posY);
+		end
+		if whichText == 4 then
+			makeLuaSprite('text', 'halloween/move', posX, posY);
+		end
+		if whichText == 5 then
+			makeLuaSprite('text', 'halloween/near', posX, posY);
+		end
+		if whichText == 6 then
+			makeLuaSprite('text', 'halloween/out', posX, posY);
+		end
+		if whichText == 7 then
+			makeLuaSprite('text', 'halloween/pal', posX, posY);
+		end
+		if whichText == 8 then
+			makeLuaSprite('text', 'halloween/punishment', posX, posY);
+		end
+		if whichText == 9 then
+			makeLuaSprite('text', 'halloween/tiring', posX, posY);
+		end
+		if whichText == 10 then
+			makeLuaSprite('text', 'halloween/voices', posX, posY);
+		end
+		if whichText == 11 then
+			makeLuaSprite('text', 'halloween/way', posX, posY);
+		end
+		setScrollFactor('text', 0.0, 0.0);
+		scaleObject('text', 1.0, 1.0);
+		addLuaSprite('text',true)
+		setObjectCamera('text','camOther')
+	end
+end
+
+function onStepHit()
+	if curStep > 671 then
+		whichText = math.random(0,11)
+		posX = math.random(0,860)
+		posY = math.random(0,650)
+		if whichText == 0 then
+			makeLuaSprite('text', 'halloween/cold', posX, posY);
+		end
+		if whichText == 1 then
+			makeLuaSprite('text', 'halloween/get_it', posX, posY);
+		end
+		if whichText == 2 then
+			makeLuaSprite('text', 'halloween/https', posX, posY);
+		end
+		if whichText == 3 then
+			makeLuaSprite('text', 'halloween/lose', posX, posY);
+		end
+		if whichText == 4 then
+			makeLuaSprite('text', 'halloween/move', posX, posY);
+		end
+		if whichText == 5 then
+			makeLuaSprite('text', 'halloween/near', posX, posY);
+		end
+		if whichText == 6 then
+			makeLuaSprite('text', 'halloween/out', posX, posY);
+		end
+		if whichText == 7 then
+			makeLuaSprite('text', 'halloween/pal', posX, posY);
+		end
+		if whichText == 8 then
+			makeLuaSprite('text', 'halloween/punishment', posX, posY);
+		end
+		if whichText == 9 then
+			makeLuaSprite('text', 'halloween/tiring', posX, posY);
+		end
+		if whichText == 10 then
+			makeLuaSprite('text', 'halloween/voices', posX, posY);
+		end
+		if whichText == 11 then
+			makeLuaSprite('text', 'halloween/way', posX, posY);
+		end
+		setScrollFactor('text', 0.0, 0.0);
+		scaleObject('text', 1.0, 1.0);
+		addLuaSprite('text',true)
+		setObjectCamera('text','camOther')
+	end
+end
+
 function noteMiss(id, noteData, noteType, isSustainNote)
 	if noteType == 'Static Note' then
 		whichText = math.random(0,11)
