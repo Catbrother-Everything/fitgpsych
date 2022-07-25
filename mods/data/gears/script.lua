ddeda06e726448cfec34f4843320555e53d35401
Lil-Parrot <chaznmouse@yahoo.com>
2022-07-25 15:10:01 -0500

    hello

--- mods/data/gears/script.lua
+++ mods/data/gears/script.lua
@@ -9,7 +9,9 @@ end
 function onSongStart()
     if Freddyg then
         playSound('Gears_Voices',1,'cock')
-        setSoundTime('cock', 280)
+        setSoundTime('cock', 221)
+		playSound('Gears_Inst',1,'h')
+        setSoundTime('h', 221)
         Freddyg = false
     end
 end
@@ -22,24 +24,29 @@ function onUpdate(elapsed)
 	L = L - 1 * elapsed
     if curStep >= 0 then
         setProperty('vocals.volume', 0)
+		setProperty('inst.volume', 0)
     end
 
 	if L == 0 then
 		setSoundVolume('cock', 1)
+		setSoundVolume('h', 1)
 	end
 end
 
 function onPause()
     pauseSound('cock')
+	pauseSound('h')
 end
 
 function onResume()
     resumeSound('cock')
+	resumeSound('h')
 end
 
 function noteMiss(id, noteData, noteType, isSustainNote)
 	if not noteType == 'Gear Note' then
 		setSoundVolume('cock', 0)
+		setSoundVolume('h', 0)
 		L = 1
 	end
 end
